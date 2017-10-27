5f361fbd084d1d99cc9c7908b34227749610f05b
MohamedAli1995 <mohamedali166@gmail.com>
2017-10-27 15:24:52 +0200

    process.cpp ( clock added )

--- process.cpp
+++ process.cpp
@@ -1,23 +1,39 @@
 
+#include<iostream>
+#include<string>
 #include "headers.h"
+#include "clkUtilities.h"
+using namespace std;
 
 /* Modify this file as needed*/
 int remainingtime =0;
-
+int stringToInt(string str);//TODO : include scheduler utilities to use function string to int 
 
 int main(int agrc, char* argv[]) {
 
     //if you need to use the emulated clock uncomment the following line
     initClk();
 
+
     //TODO: it needs to get the remaining time from somewhere
     //remainingtime = ??;
-  
+    x=getClk();
+
+  	remainingtime=stringToInt(argv[0]);
     while(remainingtime>0) {
-       sleep(1);
+       while(x==getClk());
+       x=getClk();
        remainingtime--;
     }
     //if you need to use the emulated clock uncomment the following line
-    //destroyClk(false);
+    destroyClk(false);
     return 0;
+}
+int stringToInt(string str)
+{
+    int x;
+    stringstream ss;
+    ss << str;
+    ss >> x;
+    return x;
 }
